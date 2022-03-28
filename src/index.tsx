a0f53352d1bd06c767f4707068eae780811b5abd
grisha0404 <grisha.kom@gmail.com>
2022-03-28 13:05:46 +0300

    reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {state} from "./state/state";
+import {renderTree} from "./render";
+import store from "./state/state";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App  state={state}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+store.subscribe(renderTree)
+renderTree()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
