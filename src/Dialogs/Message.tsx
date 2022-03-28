66da5b6edecc773647c7a2ab3f4b5ceddf2ad95a
grisha0404 <grisha.kom@gmail.com>
2022-03-28 13:05:42 +0300

    reducer

--- src/Dialogs/Message.tsx
+++ src/Dialogs/Message.tsx
@@ -1,41 +1,41 @@
 import React, {useState} from 'react';
 import d from './dialog.module.css'
-import {NavLink} from "react-router-dom";
 import {Dialog} from "./Dialog";
 import {NewPost} from "../Profile/NewPost";
-import {DialogsType, MessagesType} from "../state/state";
-import {v1} from "uuid";
-
+import store, {ActionType} from "../state/state";
 
 type MessageType = {
     id: string
     name: string
     ava: string
-
+    dispatch: (action: ActionType) => void
 }
-
 
 export const Message = (props: MessageType) => {
     let [collapsed, setCollapsed] = useState(false)
-    let [message, setMessage] = useState<Array<MessagesType>>([
-        {id: v1(), message: 'Hi'},
-        {id: v1(), message: 'How are you?'},
-        {id: v1(), message: 'Yo'},
-        {id: v1(), message: 'Yo'}
-    ])
+    // let [message, setMessage] = useState<Array<MessagesType>>([
+    //     {id: v1(), message: 'Hi'},
+    //     {id: v1(), message: 'How are you?'},
+    //     {id: v1(), message: 'Yo'},
+    //     {id: v1(), message: 'Yo'}
+    // ])
     const addNewMessage = (title: string) => {
-        let messageNew: MessagesType = {id: v1(), message: title}
-        setMessage([ ...message, messageNew])
+        //let messageNew: MessagesType = {id: v1(), message: title}
+        //setMessage([ ...message, messageNew])
+        props.dispatch({type: 'add-message', title: title})
     }
     return (
         <div className={d.message}>
-            <button style={{border:'none', backgroundColor:'white'}} onClick={()=> setCollapsed(!collapsed)}><img src={props.ava}/> {props.name}</button>
-            { collapsed &&
+            <button style={{border: 'none', backgroundColor: 'white'}} onClick={() => setCollapsed(!collapsed)}><img
+                src={props.ava}/> {props.name}</button>
+            {collapsed &&
                 <div>
-                    {message.map(el => <Dialog key={el.id} id={el.id} message={el.message}/>)}
-                <div className={d.inp}>
-                    <NewPost callBack={addNewMessage} name={'Send'}/>
-                </div>
+                    {store._state.messagesPage.messages.map(el => <Dialog key={el.id} id={el.id}
+                                                                          message={el.message}/>)}
+                    <div className={d.inp}>
+                        <NewPost dispatch={store.dispatch.bind(store)} callBack={(title) => store.addNewMessage(title)}
+                                 name={'Send'}/>
+                    </div>
                 </div>
             }
         </div>
