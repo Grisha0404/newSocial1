e8312b033bc8dd89998f167eceef8fc19456005c
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-17 15:06:52 +0300

    redux

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import {NewPost} from "./NewPost";
 import {Post} from "./Post";
-import store, {ActionType, PostType, ProfilePageType} from "../state/state";
+import {ProfilePageType} from "../Reducer/profilePageReducer";
 import {Info} from "./Info";
 import {Image} from "./Image";
 import s from "./post.module.css";
-import {addPostAC, propfilePageReducer} from "../reducer/profilePage-reducer";
+import {useDispatch, useSelector} from "react-redux";
+import {AppRootStateType} from "../Redux/redux-store";
+import {addPostAC} from "../Reducer/profilePageReducer";
 
-type ProfileType = {
-    posts: Array<PostType>
-    newPostText: string
-    dispatch: (action: ActionType) => void
-}
 
-export const Profile = (props: ProfileType) => {
+export const Profile = () => {
+    const profilePage = useSelector<AppRootStateType, ProfilePageType>(state => state.profilePage)
+    const dispatch = useDispatch()
     const addNewPost = (title: string) => {
-        props.dispatch({type: 'Add-post', newPostText: title})
-
+        dispatch(addPostAC(title))
     }
     return (
         <div className={'profile'}>
@@ -30,8 +28,8 @@ export const Profile = (props: ProfileType) => {
                          name={'Add post'}/>
             </div>
             <div>
-                {props.posts.map(el => <Post key={el.id} id={el.id} message={el.message}
-                                             likesCounts={el.likesCounts}/>)}
+                {profilePage.posts.map(el => <Post key={el.id} id={el.id} message={el.message}
+                                                   likesCounts={el.likesCounts}/>)}
             </div>
         </div>
     );
