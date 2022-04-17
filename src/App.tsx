cdc28ec104352b43f973b1aca42562be4d513c61
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-17 15:06:06 +0300

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import {Header} from "./Heder/Header";
 import {NavBar} from "./NavLink/NavLink";
 import {Profile} from "./Profile/Profile";
@@ -8,34 +8,27 @@ import {Dialogs} from "./Dialogs/Dialogs";
 import {News} from "./News/News";
 import {Settings} from "./Settings/Settings";
 import {Music} from "./Music/Music";
-import store, {StateType} from "./state/state";
 
-type AppType = {
-    store: StateType
-}
 
-const App: React.FC<AppType> = (props) => {
-    const state = props.store.getState()
+const App = () => {
+
     return (
-        <BrowserRouter>
-            <div className="App">
-                <Header/>
-                <NavBar/>
-                <div className={'Content'}>
+
+        <div className="App">
+            <Header/>
+            <NavBar/>
+            <div className={'Content'}>
                     <Routes>
                         <Route path='/profile'
-                               element={<Profile dispatch={store.dispatch.bind(store)} posts={state.profilePage.posts}
-                                                 newPostText={state.profilePage.newPostText}/>}/>
+                               element={<Profile/>}/>
                         <Route path='/dialogs'
-                               element={<Dialogs dispatch={store.dispatch.bind(store)} dialogs={state.messagesPage.dialogs}
-                                                 messages={state.messagesPage.messages}/>}/>
+                               element={<Dialogs/>}/>
                         <Route path='/news' element={<News/>}/>
                         <Route path='/music' element={<Music/>}/>
                         <Route path='/settings' element={<Settings/>}/>
                     </Routes>
-                </div>
             </div>
-        </BrowserRouter>
+        </div>
     );
 }
 
