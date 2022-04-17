a55704315afa6f11897f6f67df07fadf71fbd5e4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-17 15:06:28 +0300

    redux

--- src/Dialogs/Message.tsx
+++ src/Dialogs/Message.tsx
@@ -1,36 +1,16 @@
-import React, {useState} from 'react';
-import d from './dialog.module.css'
-import {Dialog} from "./Dialog";
-import {NewPost} from "../Profile/NewPost";
-import store, {ActionType} from "../state/state";
+import React from 'react';
+import {NewMessage} from "./NewMessage";
 
-type MessageType = {
+export type MessagesType = {
     id: string
-    name: string
-    ava: string
-    dispatch: (action: ActionType) => void
+    message: string
 }
 
-export const Message = (props: MessageType) => {
-    let [collapsed, setCollapsed] = useState(false)
-
-    const addNewMessage = (title: string) => {
-
-        props.dispatch({type: 'add-message', title: title})
-    }
+export const Message = (props: MessagesType) => {
     return (
-        <div className={d.message}>
-            <button style={{border: 'none', backgroundColor: 'white'}} onClick={() => setCollapsed(!collapsed)}><img
-                src={props.ava}/> {props.name}</button>
-            {collapsed &&
-                <div>
-                    {store._state.messagesPage.messages.map(el => <Dialog key={el.id} id={el.id}
-                                                                          message={el.message}/>)}
-                    <div className={d.inp}>
-                        <NewPost  callBack={addNewMessage} name={'Send'}/>
-                    </div>
-                </div>
-            }
+        <div>
+            {props.message}
         </div>
+
     );
 };
