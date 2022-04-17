06ef7648e29bb232d203f8ff7b632f20b78faecf
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-17 15:06:49 +0300

    redux

--- src/Profile/Post.tsx
+++ src/Profile/Post.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import s from './post.module.css'
 import {Like} from "./Like";
 
-type PostsType = {
+export type PostType = {
     id: string
     message: string
     likesCounts: number
 }
 
-
-export const Post = (props: PostsType) => {
+export const Post = (props: PostType) => {
     return (
         <div className={s.post}>
             <img
                 src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwxaYyNHy6PVbY0IHg6NY10JVzxiqEGuWvyQ&usqp=CAU'}/>
-            <span>{props.message}<Like/></span>
+            <span>{props.message}<Like likesCounts={props.likesCounts}/></span>
         </div>
     );
 };
