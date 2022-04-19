29f8511ba23ba68cfcd949666e71283cb40013f0
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-19 23:37:17 +0300

    ReactMemo

--- src/Dialogs/MessageContainer/Message/Message.tsx
+++ src/Dialogs/MessageContainer/Message/Message.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import {NewMessage} from "./NewMessage";
+import React, {memo} from 'react';
 
 export type MessagesType = {
     id: string
     message: string
 }
 
-export const Message = (props: MessagesType) => {
+export const Message = memo((props: MessagesType) => {
     return (
         <div>
             {props.message}
         </div>
 
     );
-};
+});
