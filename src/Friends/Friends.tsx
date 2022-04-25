0867af6e343fb3d1323f9186be99583515b1d6d4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-25 23:03:00 +0300

    users

--- src/Friends/Friends.tsx
+++ src/Friends/Friends.tsx
@@ -14,16 +14,14 @@ export type UsersType = {
 }
 
 export const Friends = () => {
-
     const [users, setUsers] = useState<UsersType[]>()
+    const [error, setError] = useState('')
 
     const usersContainer = async () => {
         try {
             const {data} = await API.usersFriends();
-            const { items, totalCount, error} = data;
-            console.log(items)
-            //setUsers(data)
-            error ? setUsers(error) :setUsers(items)
+            const { items, error} = data;
+            error ? setError(error) : setUsers(items)
         } catch (err) {
             console.log('err ', err);
         }
@@ -33,7 +31,7 @@ export const Friends = () => {
             <button onClick={usersContainer}>show</button>
             {users?.map((u, index)=> <UsersContainer key={index} name={u.name}
                                                      largePhotos={u.photos.large} smallPhotos={u.photos.small}
-                                                     status={u.status} followed={u.followed}/>)}
+                                                     status={u.status} followed={u.followed}/>) || error}
         </div>
     );
 };
