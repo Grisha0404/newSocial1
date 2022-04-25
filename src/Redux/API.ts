73be0051c8b132de8fda5959d86ea9865fa5f021
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-25 22:56:04 +0300

    users

---
+++ src/Redux/API.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import {UsersType} from "../Friends/Friends";
+import {SetStateAction} from "react";
+
+const configOMB = {
+    baseURL: 'https://social-network.samuraijs.com',
+};
+const key = '22986';
+const axiosInstance = axios.create(configOMB);
+
+const API = {
+    usersFriends: () => {
+        const query = `/api/1.0/users`;
+        return axiosInstance.get<{}, TestType<ItemsType>>(query);
+
+        //return axiosInstance.get(query).then(res => res.data);<{}, TestType<ItemsType>>
+    }
+};
+
+type TestType<T> = {
+    data: T
+}
+
+export type ItemsType = {
+    items: Array<UsersType>,
+    totalCount?: number,
+    error: SetStateAction<UsersType[] | undefined>,
+}
+
+
+export default API;
