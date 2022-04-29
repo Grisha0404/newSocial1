c9f668f0b7f27539539145a0e4858e52f7c17461
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-29 23:56:33 +0300

    profile

--- src/Profile/InfoProfile/InfoProfile.tsx
+++ src/Profile/InfoProfile/InfoProfile.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {InfoUser} from "../InfoUser";
+import API, {UsersProfileType} from "../../Redux/API";
+import {useDispatch, useSelector} from "react-redux";
+import {getUsersProfileAC} from "../../Reducer/profilePageReducer";
+import {AppRootStateType} from "../../Redux/redux-store";
 
 export const InfoProfile = () => {
+    const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const usersProfile = async () => {
+            try {
+                const {data} = await API.usersProfile();
+                dispatch(getUsersProfileAC(data))
+            } catch (err) {
+                console.log('err ', err);
+            }
+        }
+        usersProfile()
+    });
+
     return (
         <div>
             <img
                 src={'https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg'}/>
-            <InfoUser/>
+            <InfoUser profile={profile}/>
             <div style={{textAlign: 'left', padding: '20px'}}>
                 My Posts
             </div>
