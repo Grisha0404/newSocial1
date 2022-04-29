6f7172fc45a6aabfd9de306e9f35b687838bfbdc
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-29 23:56:25 +0300

    profile

--- src/Friends/User.tsx
+++ src/Friends/User.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import userPhoto from "../Common/image/userPhoto.png";
 import {UsersType} from "../Reducer/usersReducer";
 import style from './users.module.css'
+import {NavLink} from "react-router-dom";
 
 type UserType = {
     users: UsersType[]
@@ -15,8 +16,10 @@ export const User: React.FC<UserType> = ({users, callBack}) => {
             {
                 users.map(u =>
                     <div key={u.id}>
+                        <NavLink to={'/profile/' + u.id}>
                         <img src={u.photos.small !== null ? u.photos.small : userPhoto} alt={'not photo'}
                              style={{width: "60px", height: "70px"}}/>
+                        </NavLink>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
                         <button
