b11eb87490108146417c3f9df4aebafea3e58b9f
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-29 18:55:10 +0300

    usersloading

--- src/Friends/Selector.tsx
+++ src/Friends/Selector.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import s from "./users.module.css";
+import React, {useState} from 'react';
+import style from "./users.module.css";
+
 
 type SelectorType = {
     totalCount: number,
-    pageSize: number,
     callBack: (page: number) => void,
     currentPage: number,
 }
 
-export const Selector: React.FC<SelectorType> = ({totalCount, pageSize, callBack, currentPage}) => {
+export const Selector: React.FC<SelectorType> = ({totalCount, callBack, currentPage}) => {
 
-    let pageCount = Math.ceil(totalCount / pageSize);
+    const [startPages, setSmallPages] = useState(1)
+    const [endPages, setEndPages] = useState(20)
+
+    let pageCount = Math.ceil(totalCount / 20);
     let pages = [];
-    for (let i = 1; i <= pageCount; i++) {
+    for (let i = startPages; i <= endPages; i++) {
         pages.push(i)
+    }
+    const clickHandlerPlus = () => {
+        setSmallPages(startPages + 20)
+        setEndPages(endPages + 20)
+    }
+    const clickHandlerMin = () => {
+        setSmallPages(startPages - 20)
+        setEndPages(endPages - 20)
     }
 
     return (
-        <div>
-            <span>{pages.map((p, index) => <span key={index} onClick={() => callBack(p)}
-                                                 className={`${currentPage === p && s.selectedPages}`}> {p} </span>)}</span>
+        <div className={style.selectorContainer}>
+            <button onClick={clickHandlerMin} disabled={startPages<=1}>⟪⟪⟪</button>
+            <button className={style.selector} >{pages.map((p, index) => <span key={index} onClick={() => callBack(p)}
+                                                 className={`${currentPage === p && style.selectedPages}`}> {p} </span>)}</button>
+            <button onClick={clickHandlerPlus} disabled={endPages>pageCount}>⟫⟫⟫</button>
         </div>
     );
 };
