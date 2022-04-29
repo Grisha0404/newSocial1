2dbc86b19bfefcff929882fd9b2365871ed17721
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-29 23:56:29 +0300

    profile

--- src/Profile/InfoUser.tsx
+++ src/Profile/InfoUser.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {UsersProfileType} from "../Redux/API";
 
-export const InfoUser = () => {
+type InfoUserType = {
+    profile:UsersProfileType
+}
+
+export const InfoUser = (props: InfoUserType) => {
+
+
     return (
         <div>
-            <div>
-                ava + disk
-            </div>
+            <img src={props.profile.photos.small}style={{width:'120px', height: '170px'}}/>
+            <div>{props.profile.aboutMe}</div>
+            <div>{props.profile.fullName}</div>
+            <div>{props.profile.lookingForAJob}</div>
+            <div>{props.profile.lookingForAJobDescription}</div>
+            <div>{props.profile.contacts.github}</div>
         </div>
     );
 };
