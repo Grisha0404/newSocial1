10652ab591a15645df9dcab68fd46b50d501ca52
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-04-29 18:55:26 +0300

    usersloading

--- src/Friends/UsersContainer.tsx
+++ src/Friends/UsersContainer.tsx
@@ -2,47 +2,57 @@ import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../Redux/redux-store";
 import API from "../Redux/API";
-import {followAC, setSelectorAC, setUsersAC, UsersType} from "../Reducer/usersReducer";
+import {followAC, getTotalCountAC, setSelectorAC, setUsersAC, UsersType} from "../Reducer/usersReducer";
 import {User} from "./User";
 import {Selector} from "./Selector";
+import style from './users.module.css'
+import loadingImg from '../Common/image/loading__.gif'
 
 export const UsersContainer = () => {
     const users = useSelector<AppRootStateType, UsersType[]>(state => state.users.users)
-    const pageSize = useSelector<AppRootStateType, number>(state => state.users.pagesSize)
-    const [totalCount, setTotalCount] = useState<number>(1)
-    const [currentPage, setCurrentPage] = useState<number>(1)
-
+    //число кол-ва users
+    const totalCount = useSelector<AppRootStateType, number>(state => state.users.totalCount)
+    //выбраная страница
+    const currentPage = useSelector<AppRootStateType, number>(state => state.users.currentPage)
+    const [loading, setLoading] = useState(false)
     const dispatch = useDispatch();
-
-    const clickHandler = (follow: boolean, userId: number) => {
-        dispatch(followAC(follow, userId))
-    }
 
     useEffect(() => {
         const usersContainer = async () => {
             try {
-                const {data} = await API.usersFriends(currentPage, pageSize);
+                const {data} = await API.usersFriends(currentPage);
                 const {items, error, totalCount} = data;
-                setTotalCount(totalCount)
+                dispatch(getTotalCountAC(totalCount))
                 error ? dispatch(setUsersAC(error)) : dispatch(setUsersAC(items))
             } catch (err) {
                 console.log('err ', err);
+            } finally {
+                console.log("FIN")
             }
+
         }
+
         usersContainer()
     }, [dispatch, currentPage])
 
 
     const setSelectorClick = (page: number) => {
-        setCurrentPage(page)
         dispatch(setSelectorAC(page))
+    }
+    const clickHandler = (follow: boolean, userId: number) => {
+        dispatch(followAC(follow, userId))
     }
 
     return (
-        <div>
-            <Selector callBack={setSelectorClick} pageSize={pageSize} currentPage={currentPage}
-                      totalCount={totalCount}/>
+        <div className={style.usersContainer}>
+            loading ?
             <User users={users} callBack={clickHandler}/>
+            <Selector callBack={setSelectorClick} currentPage={currentPage}
+                      totalCount={totalCount}/>
+            :
+            <div>
+                <img src={loadingImg}/>
+            </div>
         </div>
     );
 };
