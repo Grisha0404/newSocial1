26262d70f36aa31d2939a4bce16d178f5f9b03f1
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-02 14:05:51 +0300

    LoginUser

--- src/Heder/Header.tsx
+++ src/Heder/Header.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {LoginContainer} from "./LoginUser/LoginContainer";
 
 export const Header = () => {
     return (
         <div className={'Header'}>
             <div>
                 <img src={'https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_1.png'}/>
+                <LoginContainer/>
             </div>
         </div>
     );
