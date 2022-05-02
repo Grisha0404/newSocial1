4bf6d427d877d540dc612fc270fb6089a34cd51a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-02 12:51:05 +0300

    userProfile

--- src/Profile/InfoProfile/InfoProfile.tsx
+++ src/Profile/InfoProfile/InfoProfile.tsx
@@ -1,31 +1,41 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {InfoUser} from "../InfoUser";
 import API, {UsersProfileType} from "../../Redux/API";
 import {useDispatch, useSelector} from "react-redux";
 import {getUsersProfileAC} from "../../Reducer/profilePageReducer";
 import {AppRootStateType} from "../../Redux/redux-store";
+import {useParams} from "react-router-dom";
+import {IsFetching} from "../../Common/IsFeatching";
+import {getFetchingAC} from "../../Reducer/usersReducer";
 
 export const InfoProfile = () => {
     const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
+    const isFetching = useSelector<AppRootStateType, boolean>(state => state.users.fetch)
     const dispatch = useDispatch()
+    //берет из url необходимую id
+    const params = useParams()
 
     useEffect(() => {
         const usersProfile = async () => {
+            dispatch(getFetchingAC(false))
             try {
-                const {data} = await API.usersProfile();
+                const {data} = await API.usersProfile(params.id)
+                dispatch(getFetchingAC(true))
                 dispatch(getUsersProfileAC(data))
             } catch (err) {
                 console.log('err ', err);
             }
         }
         usersProfile()
-    },[dispatch]);
+    }, [dispatch]);
 
     return (
         <div>
-            <img
-                src={'https://static8.depositphotos.com/1370441/848/i/600/depositphotos_8486144-stock-photo-beach-and-tropical-sea.jpg'}/>
-            <InfoUser profile={profile}/>
+            {isFetching ?
+                <InfoUser profile={profile}/>
+                :
+                <IsFetching width={'300px'} height={'200px'}/>
+            }
             <div style={{textAlign: 'left', padding: '20px'}}>
                 My Posts
             </div>
