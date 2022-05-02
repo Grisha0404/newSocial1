e9a5d7ad94c356bf234192446e74d62cc5f86075
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-02 14:05:54 +0300

    LoginUser

---
+++ src/Reducer/authUsersReducer.ts
@@ -0,0 +1,35 @@
+export type DataInitialType = {
+    id: number | null,
+    login: string | null,
+    email: string | null,
+}
+
+const initialState = {
+    id: null,
+    login: null,
+    email: null,
+}
+
+
+export const authUsersReducer = (state: DataInitialType = initialState, action: LoginActionsType) => {
+    switch (action.type) {
+        case "SET-LOGIN-USERS":
+            debugger
+            return {...state, ...action.data}
+        default:
+            return state;
+    }
+};
+export type LoginActionsType =
+    setLoginUsersACType
+
+
+type setLoginUsersACType = ReturnType<typeof setLoginUsersAC>
+
+export const setLoginUsersAC = (data:DataInitialType) => {
+    return {
+        type: 'SET-LOGIN-USERS',
+        data:data
+    } as const
+}
+
