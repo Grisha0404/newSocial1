50cf883e14c308ead8ba5875fcd8e6a6c25110f8
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-03 11:35:12 +0300

    LoginUser

--- src/Heder/LoginUser/LoginContainer.tsx
+++ src/Heder/LoginUser/LoginContainer.tsx
@@ -2,30 +2,36 @@ import React, {useEffect} from 'react';
 import API from "../../Redux/API";
 import {NavLink} from "react-router-dom";
 import style from './login.module.css'
-import {useDispatch} from "react-redux";
-import {setLoginUsersAC} from "../../Reducer/authUsersReducer";
+import {useDispatch, useSelector} from "react-redux";
+import {DataInitialType, setLoginUsersAC} from "../../Reducer/authUsersReducer";
+import {AppRootStateType} from "../../Redux/redux-store";
+import {LoginUser} from "./LoginUser";
 
 export const LoginContainer = () => {
-
+    const authUser = useSelector<AppRootStateType, DataInitialType>(state => state.login)
     const dispatch = useDispatch()
 
-    useEffect(()=>{
-        const login = async () =>{
-           try{
-               const {data} = await API.authUser()
-               debugger
-               dispatch(setLoginUsersAC(data.data))
-           } catch (error){
-
-               console.log('err')
-           }
+    useEffect(() => {
+        const login = async () => {
+            try {
+                const {data} = await API.authUser()
+                dispatch(setLoginUsersAC(data.data))
+            } catch (error) {
+                alert('Please, login!')
+            }
         }
         login()
-    },[dispatch])
+    }, [dispatch])
 
     return (
         <div className={style.loginLink}>
-            <NavLink to={'/login'}>Login</NavLink>
+            {authUser.id === null ?
+                <div>
+                    <NavLink to={'/login'}>Login</NavLink>
+                </div>
+                :
+                <LoginUser login={authUser.login} email={authUser.email}/>
+            }
         </div>
     );
 };
