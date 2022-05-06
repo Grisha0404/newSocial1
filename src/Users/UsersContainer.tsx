700dc15e0726e665a21e9d5074f2b10d71a5d655
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-06 18:44:53 +0300

    thunk

--- src/Users/UsersContainer.tsx
+++ src/Users/UsersContainer.tsx
@@ -1,8 +1,11 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {AppRootStateType} from "../Redux/redux-store";
-import API from "../Redux/API";
-import {followAC, getFetchingAC, getTotalCountAC, setSelectorAC, setUsersAC, UsersType} from "../Reducer/usersReducer";
+import {ActionType, AppRootStateType, DispatchType} from "../Redux/redux-store";
+import {
+    followUserTC, getUsersTC,
+    setSelectorAC,
+    UsersType
+} from "../Reducer/usersReducer";
 import {User} from "./User";
 import {Selector} from "./Selector";
 import style from './users.module.css'
@@ -16,24 +19,10 @@ export const UsersContainer = () => {
     const currentPage = useSelector<AppRootStateType, number>(state => state.users.currentPage)
     //loading
     const isFetching = useSelector<AppRootStateType, boolean>(state => state.users.fetch)
-
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const usersContainer = async () => {
-            dispatch(getFetchingAC(false))
-            try {
-                const {data} = await API.usersFriends(currentPage);
-                const {items, error, totalCount} = data;
-                dispatch(getFetchingAC(true))
-                dispatch(getTotalCountAC(totalCount))
-                error ? dispatch(setUsersAC(error)) : dispatch(setUsersAC(items))
-            } catch (err) {
-                console.log('err usersContainer', err);
-            }
-        }
-        usersContainer()
-
+        dispatch(getUsersTC(currentPage))
     }, [dispatch, currentPage])
 
 
@@ -41,18 +30,7 @@ export const UsersContainer = () => {
         dispatch(setSelectorAC(page))
     }
     const clickFollowHandler = (follow: boolean, userId: number) => {
-        const followUsers = async () => {
-            try {
-                follow ?
-                    await API.unFollowUsers(userId)
-                    :
-                    await API.followUsers(userId)
-            } catch (err) {
-                console.log('err followUsers', err)
-            }
-        }
-        followUsers()
-        dispatch(followAC(follow, userId))
+        dispatch(followUserTC(follow, userId))
     }
 
     return (
