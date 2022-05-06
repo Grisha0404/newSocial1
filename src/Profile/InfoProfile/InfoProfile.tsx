639c40d221a8d606b5a6781016fe65fcc39aa90a
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-07 00:12:28 +0300

    css

--- src/Profile/InfoProfile/InfoProfile.tsx
+++ src/Profile/InfoProfile/InfoProfile.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import {InfoUser} from "../InfoUser";
 import API, {UsersProfileType} from "../../Redux/API";
 import {useDispatch, useSelector} from "react-redux";
-import {getUsersProfileAC} from "../../Reducer/profilePageReducer";
+import {getUserProfile, getUsersProfileAC} from "../../Reducer/profilePageReducer";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {useParams} from "react-router-dom";
 import {IsFetching} from "../../Common/IsFeatching";
@@ -12,21 +12,12 @@ export const InfoProfile = () => {
     const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
     const isFetching = useSelector<AppRootStateType, boolean>(state => state.users.fetch)
     const dispatch = useDispatch()
-    //берет из url необходимую id
+    //берет из url id-выбранного юзера
     const params = useParams()
 
     useEffect(() => {
-        const usersProfile = async () => {
-            dispatch(getFetchingAC(false))
-            try {
-                const {data} = await API.usersProfile(params.id)
-                dispatch(getFetchingAC(true))
-                dispatch(getUsersProfileAC(data))
-            } catch (err) {
-                console.log('err usersProfile', err);
-            }
-        }
-        usersProfile()
+        dispatch(getFetchingAC(false))
+        dispatch(getUserProfile(params.id))
     }, [dispatch]);
 
     let fetchingParams = isFetching ? <InfoUser profile={profile}/> : <IsFetching width={'300px'} height={'200px'}/>
