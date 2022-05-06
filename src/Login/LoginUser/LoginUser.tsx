dc8713b0569bd167503ecc0bb12800112e6904e4
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-07 00:12:48 +0300

    css

--- src/Login/LoginUser/LoginUser.tsx
+++ src/Login/LoginUser/LoginUser.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import style from "./login.module.css";
 import ava from "../../Common/image/ava.jpeg";
 import {DataInitialType} from "../../Reducer/authUsersReducer";
+import {NavLink} from "react-router-dom";
 
 
-export const LoginUser: React.FC<DataInitialType> = ({login, email}) => {
+export const LoginUser: React.FC<DataInitialType> = ({id,login, email}) => {
 
     return (
         <div className={style.loginUser}>
+            <NavLink to={'/profile/'+id}>
             <img src={ava} alt={'Photo'}/>
+            </NavLink>
             <div className={style.loginInfo}>
                 <div>{login}</div>
                 <div className={style.block}>{email}</div>
