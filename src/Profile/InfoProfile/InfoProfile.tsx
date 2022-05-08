31799bdf95126bdef14d0b5fb1cdd0b129b31919
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:31:37 +0300

    refactor status

--- src/Profile/InfoProfile/InfoProfile.tsx
+++ src/Profile/InfoProfile/InfoProfile.tsx
@@ -1,12 +1,13 @@
-import React, {useEffect, useRef} from 'react';
-import {InfoUser} from "../InfoUser";
-import API, {UsersProfileType} from "../../Redux/API";
+import React, {useEffect, useState} from 'react';
+import {InfoUser} from "./InfoUser";
+import {UsersProfileType} from "../../Redux/API";
 import {useDispatch, useSelector} from "react-redux";
-import {currentProfileTC, getUserProfile, getUsersProfileAC} from "../../Reducer/profilePageReducer";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {useParams} from "react-router-dom";
 import {IsFetching} from "../../Common/IsFeatching";
 import {getFetchingAC} from "../../Reducer/usersReducer";
+import {StatusProfile} from "./StatusProfile/StatusProfile";
+import {getStatusProfile, getUserProfileTC} from "../../Reducer/profilePageReducer";
 
 export const InfoProfile = () => {
     const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
@@ -16,20 +17,17 @@ export const InfoProfile = () => {
     const params = useParams()
 
     useEffect(() => {
-        dispatch(currentProfileTC())
         dispatch(getFetchingAC(false))
-        dispatch(getUserProfile(params.id))
-    }, [dispatch]);
+        dispatch(getStatusProfile(params.id))
+        dispatch(getUserProfileTC(params.id))
+    }, [dispatch, params.id]);
 
     let fetchingParams = isFetching ? <InfoUser profile={profile}/> : <IsFetching width={'300px'} height={'200px'}/>
-    let pageProfiler = params.id === ':id' ? 'Yo, men! Here could be your profile!' : fetchingParams
 
     return (
         <div>
-            {pageProfiler}
-            <div style={{textAlign: 'left', padding: '20px'}}>
-                My Posts
-            </div>
+            {params.id === ':id' ? 'Yo, men! Here could be your profile!' : fetchingParams}
+            <StatusProfile/>
         </div>
     );
 };
