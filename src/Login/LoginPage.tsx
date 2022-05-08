7cadc98863f067a58b97916c0b49da20c9c1a560
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 18:56:05 +0300

    formik

--- src/Login/LoginPage.tsx
+++ src/Login/LoginPage.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import style from './LoginUser/login.module.css'
-import {LoginForm} from "./LoginForm";
+import style from './LoginUser/login.module.css';
 import {AuthWithOtherSocial} from "./AuthWithOtherSocial";
+import Login from "./LoginForm";
+
 
 export const LoginPage = () => {
+    // const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+    //
+    // const onSubmit = (formData: FormDataType) => {
+    //     console.log(formData)
+    // }
     return (
         <div className={style.loginPage}>
             <div className={style.loginContainer}>
                 <h2>Join NewSocial to hear the latest from people you follow</h2>
                 <AuthWithOtherSocial/>
                 <h4>-------- or --------</h4>
-                <LoginForm/>
+                <div className={style.login}>
+                    <Login/>
+                </div>
             </div>
         </div>
     );
