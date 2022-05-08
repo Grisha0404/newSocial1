f90deb30df42b1456789884e605503bfffa2724d
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:33:01 +0300

    refactor status

---
+++ src/Profile/InfoProfile/InfoUser.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {UsersProfileType} from "../../Redux/API";
+import userPhoto from '../../Common/image/userPhoto.png'
+import style from '../profile.module.css'
+import {ContactsProfile} from "./ContactsProfile";
+
+export type InfoUserType = {
+    profile: UsersProfileType
+}
+
+export const InfoUser = (props: InfoUserType) => {
+
+    return (
+        <div className={style.infoUserContainer}>
+            <img src={props.profile.photos.small ? props.profile.photos.small : userPhoto}
+                 style={{width: '120px', height: '170px'}} alt={'NO photo'}/>
+            <div className={style.infoUser}>
+                <div>{props.profile.aboutMe}</div>
+                <div>Name: {props.profile.fullName}</div>
+                <div>Looking for a job: {props.profile.lookingForAJob ? props.profile.lookingForAJob : 'no'}</div>
+                {props.profile.lookingForAJob ?
+                    <div>Job Description:{props.profile.lookingForAJobDescription}</div> : ''}
+                <ContactsProfile profile={props.profile}/>
+            </div>
+        </div>
+    );
+};
