35972716f46d3a69607fd639b37bd4caeb31101f
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:33:17 +0300

    refactor status

--- src/Login/LoginPage.tsx
+++ src/Login/LoginPage.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
 import style from './LoginUser/login.module.css'
-import {UniversalButton} from "../Common/UniversalButton";
+import {LoginForm} from "./LoginForm";
+import {AuthWithOtherSocial} from "./AuthWithOtherSocial";
 
 export const LoginPage = () => {
     return (
         <div className={style.loginPage}>
             <div className={style.loginContainer}>
                 <h2>Join NewSocial to hear the latest from people you follow</h2>
-                <UniversalButton callback={() => {
-                }} name={'Continue with FaceBook'}/>
-                <UniversalButton callback={() => {
-                }} name={'Continue with Google'}/>
-                <UniversalButton callback={() => {
-                }} name={'Continue with VK'}/>
+                <AuthWithOtherSocial/>
                 <h4>-------- or --------</h4>
-                <input placeholder={'Your email address or profile URL'}/>
-                <input placeholder={'Your password'}/>
-                <UniversalButton callback={() => {
-                }} name={'Sign IN'}/>
+                <LoginForm/>
             </div>
         </div>
     );
