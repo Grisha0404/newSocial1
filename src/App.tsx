359f438fb2d399aafd58c464752a400aa6faa151
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:30:09 +0300

    refactor status

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Routes, Route} from "react-router-dom";
+import {RoutesPage} from "./Routes/RoutesPage";
 import {Header} from "./Heder/Header";
 import {NavBar} from "./NavLink/NavLink";
-import {Profile} from "./Profile/Profile";
-import {Dialogs} from "./Dialogs/Dialogs";
-import {News} from "./News/News";
-import {Settings} from "./Settings/Settings";
-import {Music} from "./Music/Music";
-import {UsersContainer} from "./Users/UsersContainer";
-import {LoginPage} from "./Login/LoginPage";
 
 
-export const PATH = {
-    PROFILE: '/profile/:id',
-    DIALOGS: '/dialogs',
-    NEWS: '/news',
-    MUSIC: '/music',
-    SETTINGS: '/settings',
-    FRIENDS: '/friends',
-    LOGIN: '/login',
-}
 
 const App = () => {
 
@@ -28,19 +12,7 @@ const App = () => {
         <div className="App">
             <Header/>
             <NavBar/>
-            <div className={'Content'}>
-                <Routes>
-                    <Route path={PATH.PROFILE}
-                           element={<Profile/>}/>
-                    <Route path={PATH.DIALOGS}
-                           element={<Dialogs/>}/>
-                    <Route path={PATH.NEWS} element={<News/>}/>
-                    <Route path={PATH.MUSIC} element={<Music/>}/>
-                    <Route path={PATH.SETTINGS} element={<Settings/>}/>
-                    <Route path={PATH.FRIENDS} element={<UsersContainer/>}/>
-                    <Route path={PATH.LOGIN} element={<LoginPage/>}/>
-                </Routes>
-            </div>
+            <RoutesPage/>
         </div>
     );
 }
