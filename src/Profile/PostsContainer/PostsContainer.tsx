9c5128331a09be3a630366257139b93b772f0c08
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:34:19 +0300

    refactor status

--- src/Profile/PostsContainer/PostsContainer.tsx
+++ src/Profile/PostsContainer/PostsContainer.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import {Post} from "./Post/Post";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../Redux/redux-store";
-import {ProfilePageType} from "../../Reducer/profilePageReducer";
+import {addPostAC, ProfilePageType} from "../../Reducer/profilePageReducer";
+import s from "../profile.module.css";
+import {UniversalInput} from "../../Common/UniversalInput";
 
-export const PostsContainer = () => {
+export const PostsContainer= () => {
     const profilePage = useSelector<AppRootStateType, ProfilePageType>(state => state.profilePage)
+    const dispatch = useDispatch()
+
+    const addPost = (title: string) => {
+        dispatch(addPostAC(title))
+    }
 
     return (
         <div>
+
+            <div className={s.inp}>
+                <h3>
+                    My Posts
+                </h3>
+                <UniversalInput callback={addPost} name={'Add post'}/>
+            </div>
             {profilePage.posts.map(el => <Post key={el.id} id={el.id} message={el.message}
                                                likesCounts={el.likesCounts}/>)}
         </div>
