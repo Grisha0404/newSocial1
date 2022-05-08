6c1f2809f8109a6963c6bdd8789a5413e0bb1a82
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 18:56:02 +0300

    formik

--- src/Dialogs/MessageContainer/MessageContainer.tsx
+++ src/Dialogs/MessageContainer/MessageContainer.tsx
@@ -3,8 +3,9 @@ import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {addMessageAC, MessagesPageType} from "../../Reducer/dialogsPageReducer";
 import {Message} from "./Message/Message";
-import {UniversalInput} from "../../Common/UniversalInput";
 import d from '../dialog.module.css'
+import {AddMessageForm} from "./AddMessageForm";
+import {UniversalInput} from "../../Common/UniversalInput";
 
 export const MessageContainer = () => {
 
@@ -15,12 +16,16 @@ export const MessageContainer = () => {
         dispatch(addMessageAC(title))
     }, [dispatch])
 
+
     return (
         <div className={d.messageContainer}>
             {messages.messages.map(el => <Message key={el.id} id={el.id} message={el.message}/>)}
             <div className={d.inp}>
                 <UniversalInput callback={addNewMessage} name={'Send'}/>
+                {/*<AddMessageForm callBack={addNewMessage}/>*/}
             </div>
         </div>
     );
 };
+
+
