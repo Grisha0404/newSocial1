1b3be211ac41757d8f65847710054996f01fa06d
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:34:43 +0300

    refactor status

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import {UniversalInput} from "../Common/UniversalInput";
 import s from "./profile.module.css";
 import {InfoProfile} from "./InfoProfile/InfoProfile";
 import {PostsContainer} from "./PostsContainer/PostsContainer";
-import {useDispatch} from "react-redux";
-import {addPostAC} from "../Reducer/profilePageReducer";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../Redux/redux-store";
+import {Navigate} from 'react-router-dom';
 
 
 export const Profile = () => {
-    const dispatch = useDispatch()
-
-    const addPost = (title: string) => {
-        dispatch(addPostAC(title))
-    }
+    const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    if (!isAuth) return <Navigate to={'/login'}/>
 
     return (
         <div className={s.profile}>
             <InfoProfile/>
-            <div className={s.inp}>
-                <UniversalInput callback={addPost} name={'Add post'}/>
-            </div>
-            <PostsContainer/>
+            <PostsContainer />
         </div>
     );
 };
