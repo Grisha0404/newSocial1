eb1c64b60b0a7e1cb2a47c21ad1fb6a9ed82e8c2
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:33:21 +0300

    refactor status

--- src/Login/LoginUser/LoginUser.tsx
+++ src/Login/LoginUser/LoginUser.tsx
@@ -5,17 +5,21 @@ import {DataInitialType} from "../../Reducer/authUsersReducer";
 import {NavLink} from "react-router-dom";
 
 
-export const LoginUser: React.FC<DataInitialType> = ({id,login, email}) => {
+export const LoginUser: React.FC<DataInitialType> = ({id, login, email, isAuth}) => {
 
     return (
-        <div className={style.loginUser}>
-            <NavLink to={'/profile/'+id}>
-            <img src={ava} alt={'Photo'}/>
-            </NavLink>
-            <div className={style.loginInfo}>
-                <div>{login}</div>
-                <div className={style.block}>{email}</div>
+        isAuth ?
+            <div className={style.loginUser}>
+                <NavLink to={'/profile/' + id}>
+                    <img src={ava} alt={'Photo'}/>
+                </NavLink>
+                <div className={style.loginInfo}>
+                    <div>{login}</div>
+                    <div className={style.block}>{email}</div>
+                </div>
             </div>
-        </div>
+            : <div className={style.loginBlock}>
+                <NavLink to={'/login'}>Login</NavLink>
+            </div>
     );
 };
