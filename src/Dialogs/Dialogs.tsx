f55af33ee475fbf3649b17dbe14b357b35f96f9b
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:31:32 +0300

    refactor status

--- src/Dialogs/Dialogs.tsx
+++ src/Dialogs/Dialogs.tsx
@@ -1,11 +1,14 @@
 import React, {useCallback} from 'react';
 import d from './dialog.module.css'
 import {UniversalInput} from "../Common/UniversalInput";
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {addFriendAC} from "../Reducer/dialogsPageReducer";
 import {DialogsContainer} from "./DialogsContainer/DialogsContainer";
+import {AppRootStateType} from "../Redux/redux-store";
+import {Navigate} from "react-router-dom";
 
 export const Dialogs = () => {
+    const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
 
     const dispatch = useDispatch()
 
@@ -13,6 +16,7 @@ export const Dialogs = () => {
         dispatch(addFriendAC(title))
     }, [dispatch])
 
+    if (!isAuth) return <Navigate to={'/login'}/>
     return (
         <div className={d.dialogs}>
             <UniversalInput callback={addFriend} name={'add friends'}/>
