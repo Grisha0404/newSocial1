2ae6523f9a6a8e2dee92b3e42a0d5c29c0ae66e8
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-08 16:34:39 +0300

    refactor status

---
+++ src/Routes/RoutesPage.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Profile} from "../Profile/Profile";
+import {Dialogs} from "../Dialogs/Dialogs";
+import {News} from "../News/News";
+import {Music} from "../Music/Music";
+import {Settings} from "../Settings/Settings";
+import {UsersContainer} from "../Users/UsersContainer";
+import {LoginPage} from "../Login/LoginPage";
+
+export const PATH = {
+    PROFILE: '/profile/:id',
+    DIALOGS: '/dialogs',
+    NEWS: '/news',
+    MUSIC: '/music',
+    SETTINGS: '/settings',
+    FRIENDS: '/friends',
+    LOGIN: '/login',
+}
+
+export const RoutesPage = () => {
+    return (
+        <div className={'Content'}>
+            <Routes>
+                <Route path={PATH.PROFILE}
+                       element={<Profile/>}/>
+                <Route path={PATH.DIALOGS}
+                       element={<Dialogs/>}/>
+                <Route path={PATH.NEWS} element={<News/>}/>
+                <Route path={PATH.MUSIC} element={<Music/>}/>
+                <Route path={PATH.SETTINGS} element={<Settings/>}/>
+                <Route path={PATH.FRIENDS} element={<UsersContainer/>}/>
+                <Route path={PATH.LOGIN} element={<LoginPage/>}/>
+            </Routes>
+        </div>
+    );
+};
