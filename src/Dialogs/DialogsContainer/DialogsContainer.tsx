8e6b79fa8a203e7b5fc6601d39366dae55fd3f31
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-09 15:04:50 +0300

    refactoring

--- src/Dialogs/DialogsContainer/DialogsContainer.tsx
+++ src/Dialogs/DialogsContainer/DialogsContainer.tsx
@@ -3,12 +3,13 @@ import {Dialog} from "./Dialog/Dialog";
 import {useSelector} from "react-redux";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {MessagesPageType} from "../../Reducer/dialogsPageReducer";
+import s from '../dialog.module.css'
 
 export const DialogsContainer = () => {
     const dialogs = useSelector<AppRootStateType, MessagesPageType>(state => state.dialogsPage)
 
     return (
-        <div>
+        <div className={s.dialogContainer}>
             {dialogs.dialogs.map(el => <Dialog id={el.id} key={el.id} ava={el.ava}
                                                name={el.name}/>)}
         </div>
