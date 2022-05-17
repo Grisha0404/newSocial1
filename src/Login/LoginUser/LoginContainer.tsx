eb06743cbc8de5f451fa3147605fcfa541dc7753
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-17 19:10:57 +0300

    login

--- src/Login/LoginUser/LoginContainer.tsx
+++ src/Login/LoginUser/LoginContainer.tsx
@@ -1,18 +1,13 @@
 import React, {useEffect} from 'react';
 import style from './login.module.css'
-import {useDispatch, useSelector} from "react-redux";
-import {DataInitialType, getLoginAuthUserTC} from "../../Reducer/authUsersReducer";
+import {useSelector} from "react-redux";
+import {DataInitialType} from "../../Reducer/authUsersReducer";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {LoginUser} from "./LoginUser";
 
 export const LoginContainer = () => {
     const authUser = useSelector<AppRootStateType, DataInitialType>(state => state.login)
     const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        dispatch(getLoginAuthUserTC())
-    }, [dispatch])
 
     return (
         <div className={style.loginLink}>
