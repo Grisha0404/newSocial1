110142b64157f9e67ec893e2bbf760f739d3caeb
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-17 19:10:16 +0300

    login

--- src/Reducer/authUsersReducer.ts
+++ src/Reducer/authUsersReducer.ts
@@ -1,12 +1,12 @@
-import {Dispatch} from "redux";
 import API from "../Redux/API";
 import {AppThunk} from "../Redux/redux-store";
+import {setAppStatusAC, setErrorAC} from "./appReducer";
 
 export type DataInitialType = {
     id: number | null,
     login: string | null,
     email: string | null,
-    isAuth:boolean
+    isAuth: boolean
 }
 
 const initialState = {
@@ -20,7 +20,7 @@ const initialState = {
 export const authUsersReducer = (state: DataInitialType = initialState, action: LoginActionsType) => {
     switch (action.type) {
         case "SET-LOGIN-USERS":
-            return {...state, ...action.data, isAuth: true}
+            return {...state, ...action.data, isAuth: action.isAuth}
         default:
             return state;
     }
@@ -31,16 +31,43 @@ export type LoginActionsType =
 
 type setLoginUsersACType = ReturnType<typeof setLoginUsersAC>
 
-export const setLoginUsersAC = (data: DataInitialType) => ({type: 'SET-LOGIN-USERS', data} as const)
+export const setLoginUsersAC = (data: DataInitialType, isAuth: boolean) => ({
+    type: 'SET-LOGIN-USERS',
+    data,
+    isAuth
+} as const)
 
 export const getLoginAuthUserTC = (): AppThunk => async dispatch => {
+    dispatch(setAppStatusAC('loading'))
     try {
         const {data} = await API.authUser()
-        const {resultCode} = data
-        resultCode === 0 ?
-            dispatch(setLoginUsersAC(data.data))
-            : console.log('Error with auth login user ', data.messages)
+        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setLoginUsersAC(data.data, true))
     } catch (err) {
         console.log('Error with auth login user ', err)
     }
 }
+export const loginTC = (email: string, password: string, rememberMe: boolean): AppThunk => async (dispatch) => {
+    dispatch(setAppStatusAC('loading'))
+    try {
+        const {data} = await API.logIn(email, password, rememberMe)
+        const {resultCode} = data
+        dispatch(setAppStatusAC('succeeded'))
+        resultCode === 0 ? dispatch(getLoginAuthUserTC()) : dispatch(setErrorAC('Incorrect Email or Password!'));
+    } catch (er) {
+        dispatch(setAppStatusAC('loading'))
+        console.log(er)
+    }
+}
+export const logOut = (): AppThunk => async dispatch => {
+    dispatch(setAppStatusAC('loading'))
+    try {
+        const {data} = await API.logOut()
+        const {resultCode} = data
+        dispatch(setAppStatusAC('succeeded'))
+        resultCode === 0 && dispatch(setLoginUsersAC(initialState, false))
+    } catch (err) {
+        dispatch(setAppStatusAC('loading'))
+        console.log(err)
+    }
+};
