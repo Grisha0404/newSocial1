e9b934308f82f6e7821649eb67a2ea34becbe8f3
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-17 19:11:19 +0300

    login

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -4,6 +4,7 @@ import {dialogsPageReducer, MessagesPageReducerType} from "../Reducer/dialogsPag
 import {UsersActionsType, usersReducer} from "../Reducer/usersReducer";
 import {authUsersReducer, LoginActionsType} from "../Reducer/authUsersReducer";
 import thunk, {ThunkAction} from "redux-thunk";
+import {AppActionsType, appReducer} from "../Reducer/appReducer";
 
 
 let rootReducer = combineReducers({//сюда поместим все редьюсеры
@@ -11,6 +12,7 @@ let rootReducer = combineReducers({//сюда поместим все редью
     dialogsPage: dialogsPageReducer,
     users: usersReducer,
     login: authUsersReducer,
+    app: appReducer,
 });
 
 // определить автоматически тип всего объекта состояния
@@ -18,7 +20,12 @@ export type AppRootStateType = ReturnType<typeof rootReducer>
 // непосредственно создаём store
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
-export  type ActionType = UsersActionsType | ProfilePageReducerType | MessagesPageReducerType | LoginActionsType
+export  type ActionType =
+    UsersActionsType
+    | ProfilePageReducerType
+    | MessagesPageReducerType
+    | LoginActionsType
+    | AppActionsType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateType, unknown, ActionType>
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
