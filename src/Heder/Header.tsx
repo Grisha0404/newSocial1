86d036e5485b622561612b7c1fafb9db32b6ee30
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-17 19:10:22 +0300

    login

--- src/Heder/Header.tsx
+++ src/Heder/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import {LoginContainer} from "../Login/LoginUser/LoginContainer";
+import {LinearProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../Redux/redux-store";
 
 export const Header = () => {
+    const status = useSelector<AppRootStateType, string>(state => state.app.status)
     return (
         <div className={'Header'}>
+            {status === 'loading' && <LinearProgress/>}
             <div>
                 <img src={'https://static.rfstat.com/renderforest/images/v2/logo-homepage/line_style_1.png'}/>
                 <LoginContainer/>
             </div>
+
         </div>
     );
 };
--- src/Profile/InfoProfile/InfoProfile.tsx
+++ src/Profile/InfoProfile/InfoProfile.tsx
@@ -4,30 +4,24 @@ import {UsersProfileType} from "../../Redux/API";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {useParams} from "react-router-dom";
-import {IsFetching} from "../../Common/IsFeatching";
-import {getFetchingAC} from "../../Reducer/usersReducer";
 import {StatusProfile} from "./StatusProfile/StatusProfile";
 import {getStatusProfile, getUserProfileTC} from "../../Reducer/profilePageReducer";
 import s from '../profile.module.css'
 
 export const InfoProfile = () => {
     const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
-    const isFetching = useSelector<AppRootStateType, boolean>(state => state.users.fetch)
     const dispatch = useDispatch()
     //берет из url id-выбранного юзера
     const params = useParams()
 
     useEffect(() => {
-        dispatch(getFetchingAC(false))
         dispatch(getStatusProfile(params.id))
         dispatch(getUserProfileTC(params.id))
     }, [dispatch, params.id]);
 
-    let fetchingParams = isFetching ? <InfoUser profile={profile}/> : <IsFetching width={'300px'} height={'200px'}/>
-
     return (
         <div className={s.profileContainer}>
-            {params.id === ':id' ? 'Yo, men! Here could be your profile!' : fetchingParams}
+            {params.id === ':id' ? 'Yo, men! Here could be your profile!' :  <InfoUser profile={profile}/>}
             <StatusProfile/>
         </div>
     );
