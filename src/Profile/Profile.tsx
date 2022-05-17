8cd02cbe939f2523801dfb3f1acb118916b2574f
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-17 19:10:29 +0300

    login

--- src/Profile/Profile.tsx
+++ src/Profile/Profile.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from "./profile.module.css";
 import {InfoProfile} from "./InfoProfile/InfoProfile";
 import {PostsContainer} from "./PostsContainer/PostsContainer";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "../Redux/redux-store";
-import {Navigate} from 'react-router-dom';
+import {LoginPage} from "../Login/LoginPage";
+import {getLoginAuthUserTC} from "../Reducer/authUsersReducer";
+import {Navigate} from "react-router-dom";
 
 
 export const Profile = () => {
     const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getLoginAuthUserTC())
+    }, [dispatch])
     if (!isAuth) return <Navigate to={'/login'}/>
-
     return (
         <div className={s.profile}>
-            <InfoProfile/>
-            <PostsContainer />
+            {isAuth ?
+                <>
+                    <InfoProfile/>
+                    <PostsContainer/>
+                </>
+                :
+                <LoginPage/>
+            }
+
         </div>
     );
 };
