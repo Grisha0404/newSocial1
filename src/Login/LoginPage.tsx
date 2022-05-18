540194390e25cf72f2d19e7eed34d55a96884bf2
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-18 15:31:40 +0300

    login

--- src/Login/LoginPage.tsx
+++ src/Login/LoginPage.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import style from './LoginUser/login.module.css';
 import {AuthWithOtherSocial} from "./AuthWithOtherSocial";
 import {InnerForm} from "./LoginForm";
+import {useSelector} from "react-redux";
+import {AppRootStateType} from "../Redux/redux-store";
+import {Navigate} from "react-router-dom";
+import {UsersProfileType} from "../Redux/API";
 
 
 export const LoginPage = () => {
+    const profile = useSelector<AppRootStateType, UsersProfileType>(state => state.profilePage.profile)
+    const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
+    if (isAuth) return <Navigate to={'/profile/' + profile.userId}/>
     return (
         <div className={style.loginPage}>
             <div className={style.loginContainer}>
