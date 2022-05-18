4c506536a05e0d6d8f4cbd18183c93194b345943
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-18 15:31:00 +0300

    login

--- src/Login/LoginUser/LoginContainer.tsx
+++ src/Login/LoginUser/LoginContainer.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import style from './login.module.css'
 import {useSelector} from "react-redux";
 import {DataInitialType} from "../../Reducer/authUsersReducer";
