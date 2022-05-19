df3ea009ebbc2ebf87f60a6b2f2a0f3b5f773371
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-19 23:25:20 +0300

    login

--- src/Reducer/appReducer.tsx
+++ src/Reducer/appReducer.tsx
@@ -2,7 +2,8 @@ export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
     status: 'idle' as RequestStatusType,
-    error: null as string | null
+    error: null as string | null,
+    isInitialized:false
 }
 
 type InitialStateType = typeof initialState
@@ -13,6 +14,8 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
             return {...state, status: action.status}
         case "APP/SET-ERROR":
             return {...state, error: action.error}
+        case 'APP/SET-INITIALIZED':
+            return {...state, isInitialized: action.isInitialized}
         default:
             return state
     }
@@ -20,7 +23,9 @@ export const appReducer = (state: InitialStateType = initialState, action: AppAc
 
 export const setAppStatusAC = (status: RequestStatusType) => ({type: 'APP/SET-STATUS', status} as const);
 export const setErrorAC = (error: string | null) => ({type: 'APP/SET-ERROR', error} as const);
+export const setAppIsInitializedAC = (isInitialized: boolean) => ({type: 'APP/SET-INITIALIZED', isInitialized} as const)
 
-export type AppActionsType = SetAppStatusACType | SetErrorACType
+export type AppActionsType = SetAppStatusACType | SetErrorACType | SetAppIsInitializedACType
 type SetAppStatusACType = ReturnType<typeof setAppStatusAC>
 type SetErrorACType = ReturnType<typeof setErrorAC>
+type SetAppIsInitializedACType = ReturnType<typeof setAppIsInitializedAC>
