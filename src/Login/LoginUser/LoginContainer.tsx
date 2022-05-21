74859ef61c4b37e5418f0504d4bdc9e37edb69b7
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-21 20:06:45 +0300

    login

--- src/Login/LoginUser/LoginContainer.tsx
+++ src/Login/LoginUser/LoginContainer.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import style from './login.module.css'
 import {useSelector} from "react-redux";
-import {DataInitialType} from "../../Reducer/authUsersReducer";
 import {AppRootStateType} from "../../Redux/redux-store";
 import {LoginUser} from "./LoginUser";
+import {MeType} from "../../Redux/API";
 
 export const LoginContainer = () => {
-    const authUser = useSelector<AppRootStateType, DataInitialType>(state => state.login)
+    const authMe = useSelector<AppRootStateType, MeType>(state => state.login.data)
     const isAuth = useSelector<AppRootStateType, boolean>(state => state.login.isAuth)
 
     return (
         <div className={style.loginLink}>
-            <LoginUser login={authUser.login} email={authUser.email} id={authUser.id} isAuth={isAuth}/>
+            <LoginUser authMe={authMe} isAuth={isAuth}/>
         </div>
     );
 };
