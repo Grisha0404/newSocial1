11bd56b8f8f606b470fa11f1a30328f63b3713cd
Grisha <96186535+Grisha0404@users.noreply.github.com>
2022-05-21 20:06:38 +0300

    login

--- src/Login/LoginUser/LoginUser.tsx
+++ src/Login/LoginUser/LoginUser.tsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import style from "./login.module.css";
 import ava from "../../Common/image/ava.jpeg";
-import {DataInitialType, logOut} from "../../Reducer/authUsersReducer";
+import {logOut} from "../../Reducer/authUsersReducer";
 import {NavLink} from "react-router-dom";
 import {useDispatch} from "react-redux";
+import {MeType} from "../../Redux/API";
 
+type LoginUserType = {
+    authMe: MeType
+    isAuth: boolean,
+}
 
-export const LoginUser: React.FC<DataInitialType> = ({id, login, email, isAuth}) => {
+export const LoginUser = (props: LoginUserType) => {
     const dispatch = useDispatch()
     const outChangeHandler = () => {
         dispatch(logOut())
     }
 
     return (
-        isAuth ?
+        props.isAuth ?
             <div className={style.loginUser}>
-                <NavLink to={'/profile/' + id}>
-                    <img src={ava} alt={'Photo'}/>
-                </NavLink>
-                <div className={style.loginInfo}>
-                    <div>{login}</div>
-                    <div className={style.block}>{email}</div>
-                </div>
+                {/*<NavLink to={'/profile/' + props.authMe.id}>*/}
+                {/*    <img src={ava} alt={'Photo'}/>*/}
+                {/*</NavLink>*/}
+                {/*<div className={style.loginInfo}>*/}
+                {/*    <div>{props.authMe.login}</div>*/}
+                {/*    <div className={style.block}>{props.authMe.email}</div>*/}
+                {/*</div>*/}
                 <button onClick={outChangeHandler}>LogOut</button>
             </div>
             : <div className={style.loginBlock}>
